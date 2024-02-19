3af2d9899377588a4ac851054ec1e9cd365b6b8f
mchandu25 <139004054+mchandu25@users.noreply.github.com>
2024-02-19 15:31:19 +0530

    Update Operator.java

--- src/main/java/com/ford/onlinecustomerservicecenter/operator/Operator.java
+++ src/main/java/com/ford/onlinecustomerservicecenter/operator/Operator.java
@@ -1,4 +1,56 @@
-package com.ford.onlinecustomerservicecenter.operator;
+package com.app.customerservice.operator.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Operator {
+    @Id
+    @GeneratedValue
+    private Integer operatorId;
+    private String operatorName;
+    private Integer issuesSolvedByOperator;
+    private Integer pendingIssues;
+
+    public Operator() {
+    }
+
+    public Operator(String operatorName, Integer issuesSolvedByOperator, Integer pendingIssues) {
+        this.operatorName = operatorName;
+        this.issuesSolvedByOperator = issuesSolvedByOperator;
+        this.pendingIssues = pendingIssues;
+    }
+
+    public Integer getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Integer operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public String getOperatorName() {
+        return operatorName;
+    }
+
+    public void setOperatorName(String operatorName) {
+        this.operatorName = operatorName;
+    }
+
+    public Integer getIssuesSolvedByOperator() {
+        return issuesSolvedByOperator;
+    }
+
+    public void setIssuesSolvedByOperator(Integer issuesSolvedByOperator) {
+        this.issuesSolvedByOperator = issuesSolvedByOperator;
+    }
+
+    public Integer getPendingIssues() {
+        return pendingIssues;
+    }
+
+    public void setPendingIssues(Integer pendingIssues) {
+        this.pendingIssues = pendingIssues;
+    }
 }
