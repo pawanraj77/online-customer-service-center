988512fdea267d6602437e2547fc96f22947000b
VELAGA NANDA KIRAN <88634124+Nanda-Kiran@users.noreply.github.com>
2024-02-21 22:53:47 +0530

    Revert "Added the Code for Issue"

--- src/main/java/com/company/onlinecustomerservicecenter/issue/IssueRepository.java
+++ src/main/java/com/company/onlinecustomerservicecenter/issue/IssueRepository.java
@@ -1,9 +1,4 @@
 package com.company.onlinecustomerservicecenter.issue;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface IssueRepository extends JpaRepository<Issue, Integer> {
-    Optional<Issue> findByIssueId(Integer issueId);
+public interface IssueRepository {
 }
