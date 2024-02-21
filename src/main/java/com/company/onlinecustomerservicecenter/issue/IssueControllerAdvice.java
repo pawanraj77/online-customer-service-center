7d3933cf4c4bc10b7879e7a9242df7147123fcc2
Velaga <nvelaga@mgc1w4tg4p97.chennaigtbc.ford.com>
2024-02-21 17:21:46 +0530

    Added the Code for Issue

--- src/main/java/com/company/onlinecustomerservicecenter/issue/IssueControllerAdvice.java
+++ src/main/java/com/company/onlinecustomerservicecenter/issue/IssueControllerAdvice.java
@@ -1,4 +1,13 @@
 package com.company.onlinecustomerservicecenter.issue;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class IssueControllerAdvice {
+    @ExceptionHandler(value = {IssueException.class})
+    public ResponseEntity<String> handleIssueException(IssueException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
