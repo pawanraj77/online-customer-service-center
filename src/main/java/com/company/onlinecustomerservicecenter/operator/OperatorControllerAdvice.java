f4293cc4760cdafcb10a5e241f238622fb5d124f
mchandu25 <139004054+mchandu25@users.noreply.github.com>
2024-02-26 14:18:44 +0530

    Updated OperatorControllerAdvice.java

--- src/main/java/com/company/onlinecustomerservicecenter/operator/OperatorControllerAdvice.java
+++ src/main/java/com/company/onlinecustomerservicecenter/operator/OperatorControllerAdvice.java
@@ -1,4 +1,15 @@
 package com.company.onlinecustomerservicecenter.operator;
 
-public class OperatorControllerAdvice {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class OperatorExceptionAdvice {
+    @ExceptionHandler(value = {OperatorException.class})
+    public ResponseEntity<String>operatorException(OperatorException e)
+    {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
