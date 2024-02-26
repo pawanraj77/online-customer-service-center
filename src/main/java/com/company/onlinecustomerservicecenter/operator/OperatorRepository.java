c92b6cf2d987db2e654cfe715d270ae4b46021a9
mchandu25 <139004054+mchandu25@users.noreply.github.com>
2024-02-26 14:20:01 +0530

    Updated OperatorRepository.java

--- src/main/java/com/company/onlinecustomerservicecenter/operator/OperatorRepository.java
+++ src/main/java/com/company/onlinecustomerservicecenter/operator/OperatorRepository.java
@@ -2,5 +2,8 @@ package com.company.onlinecustomerservicecenter.operator;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OperatorRepository extends JpaRepository<Operator,Integer> {
+    Optional<Operator>findByEmail(String Email);
 }
