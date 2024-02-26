fb9b250e136fff440cc45ddf0e4a49c5308edf96
mchandu25 <139004054+mchandu25@users.noreply.github.com>
2024-02-26 14:25:48 +0530

    Created issueBucketRepository.java

---
+++ src/main/java/com/company/onlinecustomerservicecenter/operator/issueBucketRepository.java
@@ -0,0 +1,6 @@
+package com.company.onlinecustomerservicecenter.operator;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueBucketRepository extends JpaRepository<IssueBucket,Integer> {
+}
