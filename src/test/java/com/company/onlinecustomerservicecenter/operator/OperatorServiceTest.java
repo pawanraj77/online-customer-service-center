f4bb96cf39c964636431dcbc8785158a80ab09ed
mchandu25 <139004054+mchandu25@users.noreply.github.com>
2024-03-01 11:22:30 +0530

    Updated OperatorServiceTest.java

--- src/test/java/com/company/onlinecustomerservicecenter/operator/OperatorServiceTest.java
+++ src/test/java/com/company/onlinecustomerservicecenter/operator/OperatorServiceTest.java
@@ -2,6 +2,7 @@ package com.company.onlinecustomerservicecenter.operator;
 
 import com.company.onlinecustomerservicecenter.department.Department;
 import com.company.onlinecustomerservicecenter.department.DepartmentRepository;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
-public class OperatorServiceTest
+class OperatorApplicationTests
 {
     @Autowired
     private OperatorRepository operatorRepository;
@@ -33,10 +35,26 @@ public class OperatorServiceTest
         }
     }
     @Test
+    @DisplayName("Assign department to Operator")
+    void assignOperatorToDept()throws OperatorException{
+        Operator operator=null;
+        try {
+            Optional<Department>departmentOpt = this.departmentRepo.findById(1);
+            Department department=departmentOpt.get();
+            Optional<Operator>operatorOpt=this.operatorRepository.findById(52);
+            Operator operator1=operatorOpt.get();
+            operator=this.operatorService.assignOperatorToDept(operator1.getOperatorId(),department.getDeptId());
+            Assertions.assertNotNull(operator);
+        }catch (OperatorException e)
+        {
+            Assertions.fail(e.getMessage());
+        }
+    }
+    @Test
     @DisplayName("To find All operators by department id")
     void getAllOperatorsByDeptID()
     {
-        List<Operator> operatorList=null;
+        List<Operator>operatorList=null;
         try{
             operatorList=this.operatorService.getAllOperatorsByDept(1);
             Assertions.assertNotNull(operatorList);
@@ -45,4 +63,43 @@ public class OperatorServiceTest
             Assertions.fail(e.getMessage());
         }
     }
+    @Test
+    @DisplayName("Issues to be solved by operator")
+    void getRemainingIssuesByOperator()
+    {
+        Integer remainingIssues=null;
+        try{
+            remainingIssues=this.operatorService.remainingIssuesByOperator(1);
+            Assertions.assertNotNull(remainingIssues);
+        } catch (OperatorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+    @Test
+    @DisplayName("To assign issues to the operator")
+    void assignIssuesToSolve()
+    {
+        Operator operator=null;
+        try {
+            operator=this.operatorService.assignIssue(1,1);
+            Assertions.assertNotNull(operator);
+        }catch (OperatorException e)
+        {
+            Assertions.fail(e.getMessage());
+        }
+    }
+    @Test
+    @DisplayName("Operator solved the issues")
+    void operatorSolved()
+    {
+        Operator operator=null;
+        try {
+            operator=this.operatorService.issueSolved(1,1);
+            Assertions.assertNotNull(operator);
+        }catch (OperatorException e)
+        {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
 }
